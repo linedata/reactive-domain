a87da0c892a02e2c17ccff0966c1686db84731d0
Chris Condron <chris@condron.net>
2019-02-19 10:44:17 -0500

    add template class for serializer - WIP

--- src/ReactiveDomain.Foundation/StreamStore/JsonMessageSerializer.cs
+++ src/ReactiveDomain.Foundation/StreamStore/JsonMessageSerializer.cs
@@ -7,16 +7,23 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using ReactiveDomain.Messaging;
+using System;
+using System.Reflection;
+using Newtonsoft.Json.Utilities;
 
 // ReSharper disable once CheckNamespace
-namespace ReactiveDomain.Foundation {
-    public class JsonMessageSerializer : IEventSerializer {
+namespace ReactiveDomain.Foundation
+{
+    public class JsonMessageSerializer : IEventSerializer
+    {
         private static readonly JsonSerializerSettings SerializerSettings;
         public const string EventClrQualifiedTypeHeader = "EventClrQualifiedTypeName";
         public const string EventClrTypeHeader = "EventClrTypeName";
 
-        static JsonMessageSerializer() {
-            SerializerSettings = new JsonSerializerSettings {
+        static JsonMessageSerializer()
+        {
+            SerializerSettings = new JsonSerializerSettings
+            {
                 TypeNameHandling = TypeNameHandling.Auto,
                 Converters = new JsonConverter[]
                 {
@@ -24,22 +31,26 @@ namespace ReactiveDomain.Foundation {
                     new CorrelationId.CorrelationIdGuidConverter()
                 },
                 ContractResolver = new FieldBasedResolver()
-                
+
             };
         }
-        public EventData Serialize(object @event, IDictionary<string, object> headers = null) {
+        public EventData Serialize(object @event, IDictionary<string, object> headers = null)
+        {
 
-            if (headers == null) {
+            if (headers == null)
+            {
                 headers = new Dictionary<string, object>();
             }
 
-            try {
+            try
+            {
                 headers.Add(EventClrTypeHeader, @event.GetType().Name);
                 headers.Add(EventClrQualifiedTypeHeader, @event.GetType().AssemblyQualifiedName);
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 var msg = e.Message;
-                
+
             }
             var metadata = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(headers, SerializerSettings));
             var dString = JsonConvert.SerializeObject(@event, SerializerSettings);
@@ -49,7 +60,8 @@ namespace ReactiveDomain.Foundation {
             return new EventData(Guid.NewGuid(), typeName, true, data, metadata);
         }
 
-        public object Deserialize(IEventData @event) {
+        public object Deserialize(IEventData @event)
+        {
 
             var eventClrTypeName = JObject.Parse(
                                 Encoding.UTF8.GetString(@event.Metadata)).Property(EventClrQualifiedTypeHeader).Value; // todo: fallback to using type name optionally
@@ -59,28 +71,81 @@ namespace ReactiveDomain.Foundation {
                                 SerializerSettings);
         }
 
-        private class FieldBasedResolver : DefaultContractResolver
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// Creates a custom object.
+    /// </summary>
+    /// <typeparam name="T">The object type to convert.</typeparam>
+    public abstract class CustomCreationConverter<T> : JsonConverter
+    {
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            protected override IList<JsonProperty> CreateConstructorParameters(ConstructorInfo constructor, JsonPropertyCollection memberProperties)
-            {
-                return base.CreateConstructorParameters(constructor, memberProperties);
-            }
-            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization) {
-                if (type.IsSubclassOf(typeof(Message)) || type == typeof(Message)) {
-                    return base.CreateProperties(type, MemberSerialization.Fields);
-                }
-                return base.CreateProperties(type, memberSerialization);
-            }
-            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-            {
-                var props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                .Select(p => base.CreateProperty(p, memberSerialization))
-                            .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                                       .Select(f => base.CreateProperty(f, memberSerialization)))
-                            .ToList();
-                props.ForEach(p => { p.Writable = true; p.Readable = true; });
-                return props;
-            }
+            throw new NotSupportedException("CustomCreationConverter should only be used while deserializing.");
         }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+            FormatterServices.GetSafeUninitializedObject
+            T value = Create(objectType);
+            if (value == null)
+            {
+                throw new JsonSerializationException("No object created.");
+            }
+
+            serializer.Populate(reader, value);
+            return value;
+        }
+
+        /// <summary>
+        /// Creates an object which will then be populated by the serializer.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>The created object.</returns>
+        public abstract T Create(Type objectType);
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>
+        /// 	<c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="JsonConverter"/> can write JSON.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this <see cref="JsonConverter"/> can write JSON; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CanWrite => false;
     }
 }
+
